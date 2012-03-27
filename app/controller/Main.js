79bb4f001d535f95db902eb4baa11beaf21316f0
Matt McClure <matt@htbx.net>
2012-03-26 23:10:50 -0400

    got the transition working

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -15,16 +15,18 @@ Ext.define("TN.controller.Main", {
         console.log("The next button has been tapped.");
         var values = this.getInfoForm().getValues();
         console.log(values);
+
+        // We don't have a store, so create one on the fly, throw the values in there, and save it
         var Order = Ext.ModelMgr.getModel('TN.model.Order');
         var newOrder = Ext.create('TN.model.Order', {
             name: values.name,
             email: values.email,
             year: values.year,
         });
+        Ext.Viewport.setActiveItem(Ext.create('TN.view.Items')); // This will eventually need to be inside the success stuff, but we just want to test the new page
         newOrder.save({
             success: function(ed) {
                 console.log("Saved the order! Its ID is "+ newOrder.getId());
-                
             }
         });
     },
